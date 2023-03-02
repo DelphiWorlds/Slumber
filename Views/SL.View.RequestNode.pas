2ded30a88b5b9dc77728c414058121c8874371ec
Dave Nottage <davidn@radsoft.com.au>
2023-03-02 18:32:01 +1030

    RequestNodeView tweaks

--- Views/SL.View.RequestNode.pas
+++ Views/SL.View.RequestNode.pas
@@ -3,22 +3,28 @@ unit SL.View.RequestNode;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
-  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls, FMX.Controls.Presentation;
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls, FMX.Controls.Presentation, FMX.Edit,
+  SL.NodeEdit;
 
 type
   TRequestNodeView = class(TFrame)
     MethodLabel: TLabel;
-    DescriptionLabel: TLabel;
+    DescriptionEdit: TEdit;
   private
+    FID: string;
+    FOnDescriptionChange: TNotifyEvent;
     function GetDescription: string;
     function GetHTTPMethod: string;
     procedure SetDescription(const Value: string);
     procedure SetHTTPMethod(const Value: string);
   public
     constructor Create(AOwner: TComponent); override;
+    procedure GainFocus;
+    property ID: string read FID write FID;
     property Description: string read GetDescription write SetDescription;
     property HTTPMethod: string read GetHTTPMethod write SetHTTPMethod;
+    property OnDescriptionChange: TNotifyEvent read FOnDescriptionChange write FOnDescriptionChange;
   end;
 
 implementation
@@ -31,11 +37,18 @@ constructor TRequestNodeView.Create(AOwner: TComponent);
 begin
   inherited;
   Name := '';
+  SetDescription('');
+  SetHTTPMethod('');
+end;
+
+procedure TRequestNodeView.GainFocus;
+begin
+  DescriptionEdit.SetFocus;
 end;
 
 function TRequestNodeView.GetDescription: string;
 begin
-  Result := DescriptionLabel.Text;
+  Result := DescriptionEdit.Text;
 end;
 
 function TRequestNodeView.GetHTTPMethod: string;
@@ -45,7 +58,7 @@ end;
 
 procedure TRequestNodeView.SetDescription(const Value: string);
 begin
-  DescriptionLabel.Text := Value;
+  DescriptionEdit.Text := Value;
 end;
 
 procedure TRequestNodeView.SetHTTPMethod(const Value: string);
