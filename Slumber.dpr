d80b7cadbe9693afcc50246bee0511a9ce00e6bd
Dave Nottage <davidn@radsoft.com.au>
2023-03-03 23:07:23 +1030

    Project changes

--- Slumber.dpr
+++ Slumber.dpr
@@ -6,21 +6,22 @@ program Slumber;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  SL.View.Main in 'Views\SL.View.Main.pas' {MainView},
-  SL.View.Header in 'Views\SL.View.Header.pas' {HeaderView: TFrame},
-  SL.Types in 'Core\SL.Types.pas',
-  SL.Consts in 'Core\SL.Consts.pas',
   SL.Client in 'Core\SL.Client.pas',
-  SL.Resources in 'Core\SL.Resources.pas' {Resources: TDataModule},
+  SL.Consts in 'Core\SL.Consts.pas',
   SL.Model.Profile in 'Models\SL.Model.Profile.pas',
+  SL.NodeEdit in 'Core\SL.NodeEdit.pas',
+  SL.Resources in 'Core\SL.Resources.pas' {Resources: TDataModule},
   SL.Storage.Profile in 'Storage\SL.Storage.Profile.pas',
-  SL.View.RequestNode in 'Views\SL.View.RequestNode.pas' {RequestNodeView: TFrame},
+  SL.Types in 'Core\SL.Types.pas',
   SL.View.FolderNode in 'Views\SL.View.FolderNode.pas' {FolderNodeView: TFrame},
-  SL.NodeEdit in 'Core\SL.NodeEdit.pas';
+  SL.View.Header in 'Views\SL.View.Header.pas' {HeaderView: TFrame},
+  SL.View.Main in 'Views\SL.View.Main.pas' {MainView},
+  SL.View.RequestNode in 'Views\SL.View.RequestNode.pas' {RequestNodeView: TFrame};
 
 {$R *.res}
 
 begin
+  ReportMemoryLeaksOnShutdown := True;
   Application.Initialize;
   Application.CreateForm(TResources, Resources);
   Application.CreateForm(TMainView, MainView);
