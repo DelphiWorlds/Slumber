8f33a738a67f3b4c6908d4e5a4c8385b8c72bed3
Dave Nottage <davidn@radsoft.com.au>
2023-06-26 14:35:19 +0930

    Removed SVGIconImageList

--- Slumber.dpr
+++ Slumber.dpr
@@ -10,21 +10,21 @@ uses
   SL.Consts in 'Core\SL.Consts.pas',
   SL.Model.Profile in 'Models\SL.Model.Profile.pas',
   SL.NodeEdit in 'Core\SL.NodeEdit.pas',
-  SL.Resources in 'Core\SL.Resources.pas' {Resources: TDataModule},
   SL.Storage.Profile in 'Storage\SL.Storage.Profile.pas',
   SL.Types in 'Core\SL.Types.pas',
   SL.View.FolderNode in 'Views\SL.View.FolderNode.pas' {FolderNodeView: TFrame},
   SL.View.Header in 'Views\SL.View.Header.pas' {HeaderView: TFrame},
   SL.View.Main in 'Views\SL.View.Main.pas' {MainView},
   SL.View.RequestNode in 'Views\SL.View.RequestNode.pas' {RequestNodeView: TFrame},
-  SL.Config in 'Core\SL.Config.pas';
+  SL.Config in 'Core\SL.Config.pas',
+  SL.Resources in 'Core\SL.Resources.pas',
+  SL.SVGResources in 'Core\SL.SVGResources.pas' {SVGResources: TDataModule};
 
 {$R *.res}
 
 begin
   ReportMemoryLeaksOnShutdown := True;
   Application.Initialize;
-  Application.CreateForm(TResources, Resources);
   Application.CreateForm(TMainView, MainView);
   Application.Run;
 end.
